5a7961558c424ace5b18695e594305272eea218e
Lukas_Beck_DITF <lukas.beck@ditf.de>
2022-10-26 10:18:01 +0200

    support for direct settings

--- lib/Settings/Settings.cpp
+++ lib/Settings/Settings.cpp
@@ -10,73 +10,68 @@ void Settings::setup() {
   SHUTDOWN_TIME = preferences.getInt("SHUTDOWN_TIME", SHUTDOWN_TIME);
   SHUTDOWN_THRESHOLD = preferences.getInt("SHUTDOWN_THRESHOLD", SHUTDOWN_THRESHOLD);
   BUF_LEN = preferences.getInt("BUF_LEN", BUF_LEN);
-  SENSOR_MODE = preferences.getString("SENSOR_MODE", SENSOR_MODE);
   CONTROLLER_MODE = preferences.getString("mode", CONTROLLER_MODE);
 
   print_settings();
   Serial.println("To change controller settings send anything.");
   unsigned long time = millis();
   bool set_settings = false;
-  while (millis() < time + 2000) {
-    if (Serial.available() > 0){
-      while(Serial.available() > 0){
-        Serial.read();
-      }
-      set_settings = true;
-      break;
+
+  while (set_settings || millis() < time + 2000) {
+
+    if (millis() < time + 2000) { // check for input
+      if (Serial.available() > 0) set_settings = true;
+      else continue;
+
+    } else {
+      print_settings();
+      Serial.print("To change a setting (Only settings with only uppercase letters), input the exact name, to exit enter EXIT: ");
+      while (Serial.available() == 0) {delay(1);} // wait for input
     }
-  }
-
-  while (set_settings) {
-    print_settings();
-    Serial.print("To change a setting (Only settings with only uppercase letters), input the exact name, to exit enter EXIT: ");
-
-    while (Serial.available() == 0) {delay(1);} // wait for input
+    
     String input = Serial.readString();
     input.toUpperCase();
     Serial.println(input);
     
-    if (input.equals("ID")) {
-        ID = change_setting(input);
+    if (input.startsWith("ID")) {
+      ID = change_setting(input);
 
-    } else if (input.equals("SEND_FREQ")){
-        SEND_FREQ = change_setting(input, " in ms");
+    } else if (input.startsWith("SEND_FREQ")){
+      SEND_FREQ = change_setting(input, " in ms");
 
-    } else if (input.equals("SHUTDOWN_TIME")){
-        SHUTDOWN_TIME = change_setting(input, " in s");
+    } else if (input.startsWith("SHUTDOWN_TIME")){
+      SHUTDOWN_TIME = change_setting(input, " in s");
 
-    } else if (input.equals("SHUTDOWN_THRESHOLD")){
-        SHUTDOWN_THRESHOLD = change_setting(input, " between 0 and 4096");
+    } else if (input.startsWith("SHUTDOWN_THRESHOLD")){
+      SHUTDOWN_THRESHOLD = change_setting(input, " between 0 and 4096");
 
-    } else if (input.equals("BUF_LEN")){
-        BUF_LEN = change_setting(input);
-
-    } else if (input.equals("SENSOR_MODE")){
-        Serial.print("Setting SENSOR_Mode, please enter new mode: Mat or Ribbon ");
-        while (Serial.available() == 0) {delay(1);} // wait for input
-        String value = Serial.readString();
-        Serial.println(value);
-        if (value == "Mat" || "Ribbon") {
-          preferences.putString(input.c_str(), value);
-          SENSOR_MODE = value;
-        } else Serial.println("Not correct try again");
+    } else if (input.startsWith("BUF_LEN")){
+      BUF_LEN = change_setting(input);
 
     } else if (input.equals("EXIT")) {
-        Serial.println("Exiting Settings...");
-        set_settings = false;
+      Serial.println("Exiting Settings...");
+      set_settings = false;
 
     } else {
       Serial.println("Entered setting not found please try again.");
     }
   }
+  print_settings();
 }
 
 
-int Settings::change_setting(String input, String unit){
-  Serial.print("Setting " + input + unit +", please enter new value: ");
-  while (Serial.available() == 0) {delay(1);} // wait for input
-  int value = Serial.readString().toInt();
-  Serial.println(value);
+int Settings::change_setting(String input, String unit, int _value){
+  int index = input.indexOf(" ");
+  int value;
+  if (index != -1) {
+    value = input.substring(index+1).toInt();
+    input.remove(index);
+  } else {
+    Serial.print("Setting " + input + unit +", please enter new value: ");
+    while (Serial.available() == 0) {delay(1);} // wait for input
+    value = Serial.readString().toInt();
+    Serial.println(value);
+  }
   preferences.putInt(input.c_str(), value);
   return value;
 }
@@ -90,8 +85,7 @@ void Settings::print_settings(){
     "\n SEND_FREQ:          " + String(SEND_FREQ) + 
     "\n SHUTDOWN_TIME:      " + String(SHUTDOWN_TIME) + 
     "\n SHUTDOWN_THRESHOLD: " + String(SHUTDOWN_THRESHOLD) +
-    "\n BUF_LEN:            " + String(BUF_LEN) +
-    "\n SENSOR_MODE:        " + String(SENSOR_MODE)
+    "\n BUF_LEN:            " + String(BUF_LEN)
   );
 }
 
--- lib/Settings/Settings.h
+++ lib/Settings/Settings.h
@@ -17,7 +17,7 @@ class Settings {
   Preferences preferences;
 
   // get new value for setting, set it in flash storage and returns value
-  int change_setting(String input, String unit="");
+  int change_setting(String input, String unit="", int value=0);
   
 
   public:
@@ -26,7 +26,7 @@ class Settings {
   int SHUTDOWN_TIME = 300; // time after the controller turns of in s
   int SHUTDOWN_THRESHOLD = 400; // read range that has to be exceeded so that the automatic shutdown time resets
   int BUF_LEN = 10; // length of buffer (more increases time for reading sensor)
-  String SENSOR_MODE = "Mat"; // sensor for game controller Mat or Ribbon
+ 
   String CONTROLLER_MODE = "BLE_VALUES"; // holds the current Mode: BLE_VALUES, SERIAL_BT_VALUES, GAMEPAD
 
   // set up settings and waits for input
